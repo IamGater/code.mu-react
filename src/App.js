6fc9f851ac98d624e3477d2782853cf2413a416c
IamGater <116513307+IamGater@users.noreply.github.com>
2025-03-02 09:08:05 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import "./App.css";
+//
+//
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       text
+//       <p className="p1">1</p>
+//       <p className="p2">2</p>
+//     </div>
+//   );
+// }
+//
+// function App() {
+//   return (
+//     <ul>
+//       <li>1</li>
+//       <li>2</li>
+//       <li>3</li>
+//       <li>4</li>
+//     </ul>
+//   );
+// }
+//
+// function App() {
+//   const arr = [1, 2, 3, 4, 5];
+
+//   return (
+//     <ul>
+//       <li>{arr[0]}</li>
+//       <li>{arr[1]}</li>
+//       <li>{arr[2]}</li>
+//       <li>{arr[3]}</li>
+//     </ul>
+//   );
+// }
+//
+//
+// function App() {
+//   const obj = { name: "john", surname: "smit" };
+
+//   return (
+//     <p>
+//       name: <span>{obj.name}</span>, <br />
+//       surname: <span>{obj.surname}</span>,
+//     </p>
+//   );
+// }
+//
+// function App() {
+// 	const attr = 'block';
+
+// 	return <div id={attr}>
+// 		text
+// 	</div>;
+// }
+// //
+// function App() {
+//   const li1 = <li>text1</li>;
+//   const li2 = <li>text2</li>;
+//   const li3 = <li>text3</li>;
+//   return <ul>
+//     {li1}
+//     {li2}
+//     {li3}
+//   </ul>;
+// }
+//
+// function App() {
+//   const items = (
+//     <ul>
+//       <li>text1</li>
+//       <li>text2</li>
+//       <li>text3</li>
+//     </ul>
+//   );
+
+//   return {items};
+// }
+//
+function App() {
+  return (
+    <div>
+      <input /> <br />
+      <input /> <br />
+      <input /> <br />
+    </div>
+  );
+}
+//
+//
+//
+//
+//
+export default App;
