ce4cb70d0872235fd636fbfe023f48d22c25ea70
IamGater <116513307+IamGater@users.noreply.github.com>
2025-03-19 10:36:59 +0200

    1

--- src/App.js
+++ src/App.js
@@ -79,14 +79,125 @@ import "./App.css";
 //   return {items};
 // }
 //
+// function App() {
+//   return (
+//     <div>
+//       <input /> <br />
+//       <input /> <br />
+//       <input /> <br />
+//     </div>
+//   );
+// }
+//
+// function App() {
+//   return (
+//     <ul>
+//      <li>1</li>
+//      <li>2</li>
+//      <li>3</li>
+//      <li>4</li>
+//      <li>5</li>
+//      <li>6</li>
+//      <li>7</li>
+//      <li>8</li>
+//      <li>9</li>
+//      <li>10</li>
+//     </ul>
+//   );
+// }
+//
+// function App() {
+// 	return <table>
+// 		<tr>
+// 			<td>1</td>
+// 			<td>2</td>
+// 			<td>3</td>
+// 		</tr>
+// 		<tr>
+// 			<td>4</td>
+// 			<td>5</td>
+// 			<td>6</td>
+// 		</tr>
+//     <tr>
+// 			<td>7</td>
+// 			<td>8</td>
+// 			<td>8</td>
+// 		</tr>
+// 	</table>;
+// }
+//
+// function App() {
+//   const isAdult = true;
+//
+//   return <>{isAdult ? <p>isAdult = true</p> : <p>isAdult = false</p>}</>;
+// }
+//
+// function App() {
+//   const isAdmin = true;
+//
+//   return <>{isAdmin ? <p>isAdmin = true</p> : <p>isAdmin = false</p>}</>;
+// }
+//
+// function App() {
+//   let isAdult = false;
+//   const age = 19;
+//   if (age >= 18) {
+//     isAdult = true;
+//   }
+//
+//   return <>{isAdult ? <p>isAdult = true</p> : <p>isAdult = false</p>}</>;
+// }
+//
+// function App() {
+//   const isAuth = true;
+//
+//   return <>{isAuth ? <p>isAuth = true</p> : <p>isAuth = false</p>}</>;
+// }
+//
+// function App() {
+//   const getDigitsSum = (num) => {
+//     return num
+//       .toString()
+//       .split("")
+//       .map(Number)
+//       .reduce((acc, digit) => acc + digit, 0);
+//   };
+//
+//   return <>{getDigitsSum(12345)}</>;
+// }
+// function App() {
+// 	function show1() {
+// 		alert(1);
+// 	}
+//
+// 	function show2() {
+// 		alert(2);
+// 	}
+//	
+// 	return <div>
+// 		<button onClick={show1}>act1</button>
+// 		<button onClick={show2}>act2</button>
+// 	</div>;
+// }
+//
+// function App() {
+// 	function func(event) {
+// 		console.log(event);
+// 	}
+	
+// 	return <div>
+// 		<button onClick={func}>act</button>
+// 	</div>;
+// }
+//
 function App() {
-  return (
-    <div>
-      <input /> <br />
-      <input /> <br />
-      <input /> <br />
-    </div>
-  );
+	function func(event) {
+		console.log(event.target);
+	}
+	
+	return <div>
+		<button onClick={func}>act</button>
+	</div>;
 }
 //
 //
