494231f43d7392d6607dbc3711446b081017e3d3
IamGater <116513307+IamGater@users.noreply.github.com>
2025-06-23 19:42:54 +0300

    1

--- src/App.js
+++ src/App.js
@@ -639,7 +639,85 @@ import "./App.css";
 //
 //
 //
-//  
+//
+// function App() {
+//   const [sum, setSum] = useState(0);
+//
+//   return (
+//     <>
+//       <input
+//         onBlur={(e) =>
+//           setSum([...e.target.value].reduce((a, c) => a + (+c || 0), 0))
+//         }
+//       />
+//       <p>sum: {sum}</p>
+//     </>
+//   );
+// }
+//
+//
+//
+//
+// import { useState } from "react";
+
+// function App() {
+//   const [text, setText] = useState("");
+
+//   const translit = (str) =>
+//     str.replace(/[А-яёЁ]/g, (char) => {
+//       const map = {
+//         а: "a",
+//         б: "b",
+//         в: "v",
+//         г: "g",
+//         д: "d",
+//         е: "e",
+//         ё: "yo",
+//         ж: "zh",
+//         з: "z",
+//         и: "i",
+//         й: "y",
+//         к: "k",
+//         л: "l",
+//         м: "m",
+//         н: "n",
+//         о: "o",
+//         п: "p",
+//         р: "r",
+//         с: "s",
+//         т: "t",
+//         у: "u",
+//         ф: "f",
+//         х: "kh",
+//         ц: "ts",
+//         ч: "ch",
+//         ш: "sh",
+//         щ: "shch",
+//         ъ: "",
+//         ы: "y",
+//         ь: "",
+//         э: "e",
+//         ю: "yu",
+//         я: "ya",
+//       };
+//       const lower = char.toLowerCase();
+//       const isUpper = char !== lower;
+//       const tr = map[lower] || char;
+//       return isUpper ? tr.charAt(0).toUpperCase() + tr.slice(1) : tr;
+//     });
+
+//   return (
+//     <div>
+//       <textarea onChange={(e) => setText(e.target.value)} />
+//       <p>{translit(text)}</p>
+//     </div>
+//   );
+// }
+//
+//
+//
+
+//
 //
 //
 //
